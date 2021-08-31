a59013d41752540ee600f8037a43d092a9fda9d7
preccrep <3456024059@qq.com>
2021-08-31 01:44:48 -0700

    regular expressions

--- user/find.c
+++ user/find.c
@@ -3,6 +3,45 @@
 #include "kernel/fs.h"
 #include "kernel/stat.h"
 
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
+
 char*
 fmtname(char *path)
 {
@@ -42,6 +81,8 @@ find(char *path, char *filename) {
     switch(st.type) {
     case T_FILE:
         if(strcmp(fmtname(path), filename) == 0)
+            printf("%s\n", path);
+        else if((filename[0] == '^' || filename[0] == '$' || filename[0] == '.') && match(filename, fmtname(path)))
             printf("%s\n", path);
         // else {
         //     printf("no: path:%s  filename:%s\n", fmtname(path), filename);
