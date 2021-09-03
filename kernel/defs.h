e086c86709f6b11406c8040770cc8cafe0cdf244
preccrep <3456024059@qq.com>
2021-09-02 22:48:04 -0700

    switch to branch thread

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,8 +63,6 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-int             cow_alloc(pagetable_t, uint64);
-void            increase_rc(uint64);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -158,7 +156,6 @@ int             uartgetc(void);
 // vm.c
 void            kvminit(void);
 void            kvminithart(void);
-uint64          kvmpa(uint64);
 void            kvmmap(uint64, uint64, uint64, int);
 int             mappages(pagetable_t, uint64, uint64, uint64, int);
 pagetable_t     uvmcreate(void);
@@ -173,7 +170,6 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-pte_t *         walk(pagetable_t, uint64, int);
 
 // plic.c
 void            plicinit(void);
